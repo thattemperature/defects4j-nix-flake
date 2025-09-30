7cf4d3a9e04d8e30f2898a6abffe20e5f0c5b513
thattemperature <2719023332@qq.com>
2025-09-30 11:35:49 +0800

    Add "flake-utils" and update "flake.lock".

--- flake.nix
+++ flake.nix
@@ -4,47 +4,50 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils/main";
   };
 
   outputs =
-    { nixpkgs, ... }:
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
 
-    let
+      let
 
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs { inherit system; };
 
-      perl-pkgs =
-        ps: with ps; [
-          DBDCSV
-          DBI
-          JSON
-          JSONParse
-          ListAllUtils
-          PerlCritic
-          StringInterpolate
-          URI
+        perl-pkgs =
+          ps: with ps; [
+            DBDCSV
+            DBI
+            JSON
+            JSONParse
+            ListAllUtils
+            PerlCritic
+            StringInterpolate
+            URI
+          ];
+        perl-env = pkgs.perl.withPackages perl-pkgs;
+        java = pkgs.jdk11;
+        basics = with pkgs; [
+          curl
+          unzip
         ];
-      perl-env = pkgs.perl.withPackages perl-pkgs;
-      java = pkgs.jdk11;
-      basics = with pkgs; [
-        curl
-        unzip
-      ];
 
-    in
+      in
 
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        name = "defects4j-shell";
+      {
+        devShells.default = pkgs.mkShell {
+          name = "defects4j-shell";
 
-        buildInputs = [
-          perl-env
-          java
-        ]
-        ++ basics;
+          buildInputs = [
+            perl-env
+            java
+          ]
+          ++ basics;
 
-      };
-    };
+        };
+      }
+    );
 
 }
